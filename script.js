0e93f36df39d018262f3c5dc796cf11a428ea02d
Mohammed Ansari <112924825+Mohammedansari808@users.noreply.github.com>
2022-10-19 13:54:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,54 @@
+
+
+/////// CALL BACK HELL ////////
+
+let div = document.createElement("div");
+div.style.display='flex';
+div.style.justifyContent="center"
+div.style.marginTop="300px"
+div.style.fontSize="500%"
+let second = 10;
+div.innerHTML=second;
+document.body.append(div)
+setTimeout(() => {
+    second -= 1
+    div.innerHTML=second
+    setTimeout(() => {
+        second -= 1
+        div.innerHTML=second
+        setTimeout(() => {
+            second -= 1
+            div.innerHTML=second
+            setTimeout(() => {
+                second -= 1
+                div.innerHTML=second
+                setTimeout(() => {
+                    second -= 1
+                    div.innerHTML=second
+                    setTimeout(() => {
+                        second -= 1
+                        div.innerHTML=second
+                        setTimeout(() => {
+                            second -= 1
+                            div.innerHTML=second
+                            setTimeout(() => {
+                                second -= 1
+                                div.innerHTML=second
+                                setTimeout(() => {
+                                    second -= 1
+                                    div.innerHTML=second
+                                    setTimeout(() => {
+                                        second -= 1
+                                        div.innerHTML="Happy Independence Day"
+                                        
+                                    }, 1000);
+                                }, 1000); 
+                            }, 1000);
+                        }, 1000);
+                    }, 1000);
+                }, 1000);  
+            }, 1000);
+        }, 1000);
+    }, 1000);
+}, 1000);
+
